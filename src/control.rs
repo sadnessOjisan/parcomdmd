76c65b98356de634ac932db64355eb8cb2b29c13
sadness_ojisan <sadness.ojisan@gmail.com>
2022-01-23 16:12:50 +0900

    fmap?

--- src/control.rs
+++ src/control.rs
@@ -14,6 +14,25 @@ fn map(f: Box<dyn Fn(i32) -> i32>, a: i32) -> i32 {
     3
 }
 
+// haskell
+// class Functor f where
+//   fmap :: (a -> b) -> f a -> f b
+pub trait Functor<T, U> {
+    type Out;
+
+    fn fmap<F: Fn(&T) -> U>(&self, F) -> Self::Out;
+}
+
+// Arity 1
+impl<T, U> Functor<T, U> for (T,)  {
+    type Out = (U,);
+
+    #[inline(always)]
+    fn fmap<F: Fn(&T) -> U>(&self, f: F) -> Self::Out {
+        (f(&self.0),)
+    }
+}
+
 pub fn pure() {}
 
 pub fn apply() {}
